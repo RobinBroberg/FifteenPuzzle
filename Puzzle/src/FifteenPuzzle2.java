dca4c6f15320db62abe6a97fa8d3f8490342dc1a
RobinBroberg <robin.broberg86@gmail.com>
2022-10-30 22:37:52 +0100

    changes

--- Puzzle/src/FifteenPuzzle2.java
+++ Puzzle/src/FifteenPuzzle2.java
@@ -1,29 +1,31 @@
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 import javax.swing.*;
 
 public class FifteenPuzzle2 extends JFrame implements ActionListener {
     private final JButton[] buttons;
-    private final JPanel panel = new JPanel();
+    private final JPanel mainPanel = new JPanel();
+    private final JPanel southPanel = new JPanel();
     private final JButton resetButton = new JButton("RESET");
+    private final JButton winButton = new JButton("WIN");
     private final int randomOne = ThreadLocalRandom.current().nextInt(1, 16);
-
-    // protected List<JButton> list = new ArrayList<>();
 
     public FifteenPuzzle2() {
 
         setTitle("FifteenPuzzle");
-        panel.setLayout(new GridLayout(4, 4));
+        mainPanel.setLayout(new GridLayout(4, 4));
         buttons = makeButtons();
-        add(panel);
-        add(resetButton, BorderLayout.SOUTH);
+        add(mainPanel);
+        add(southPanel, BorderLayout.SOUTH);
+        southPanel.add(resetButton);
+        southPanel.add(winButton);
         resetButton.addActionListener(this);
-        panel.setBackground(Color.BLACK);
+        winButton.addActionListener(this);
+        mainPanel.setBackground(Color.BLACK);
         pack();
         setSize(500, 500);
         setVisible(true);
@@ -35,20 +37,20 @@ public class FifteenPuzzle2 extends JFrame implements ActionListener {
 
     public void mixAndAddButtons() {
         if (buttons != null) {
-            Collections.shuffle(List.of(buttons));
-            for (JButton button : buttons) {
-                panel.add(button);
-                panel.revalidate();
-                panel.repaint();
+            Collections.shuffle(Arrays.asList(buttons));
+            for (int i = 0; i < buttons.length; i++) {
+                mainPanel.add(buttons[i]);
+                mainPanel.revalidate();
+                mainPanel.repaint();
             }
         }
-
     }
+
 
     public JButton[] makeButtons() {
         JButton[] buttons = new JButton[16];
         int j = 1;
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < buttons.length; i++) {
             if (i == randomOne) {
                 JButton hiddenButton = new JButton();
                 buttons[i] = hiddenButton;
@@ -73,6 +75,9 @@ public class FifteenPuzzle2 extends JFrame implements ActionListener {
                 buttons[i].setBackground(Color.CYAN);
             }
         }
+        if (e.getSource()==winButton){
+
+        }
 
         if (e.getSource() instanceof JButton && e.getSource() != resetButton) {
             ((JButton) e.getSource()).setBackground(Color.RED);
@@ -80,7 +85,7 @@ public class FifteenPuzzle2 extends JFrame implements ActionListener {
     }
 
     public static void main(String[] args) {
-        new FifteenPuzzle();
+        new FifteenPuzzle2();
 
     }
 }
